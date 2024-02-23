f4057363af1ea12945999ba2e3feb50a346e1ac4
sakhb <druzhininsergey96@gmail.com>
2024-02-24 07:28:51 +1100

    RA-04 fragment categories

--- app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipeapp/CategoriesListFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.recipeapp.databinding.ActivityMainBinding
 import com.example.recipeapp.databinding.FragmentListCategoriesBinding
 
 class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
@@ -13,8 +14,7 @@ class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = FragmentListCategoriesBinding.inflate(inflater)
+        val binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding.root
-//        return inflater.inflate(R.layout.fragment_list_categories, container, false)
     }
 }
--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.recipeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import com.example.recipeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +15,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        supportFragmentManager.beginTransaction().commit()
+        val fragment: Fragment = CategoriesListFragment()
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.add(R.id.mainContainer, fragment).commit()
     }
 }
