0640ba265e0df1e38658b30c54a0e1a7b37ff691
sakhb <druzhininsergey96@gmail.com>
2024-05-01 05:54:52 +1100

    RA-32 add @Serializable to Category class

--- app/src/main/java/com/example/recipeapp/model/Compotents.kt
+++ app/src/main/java/com/example/recipeapp/model/Compotents.kt
@@ -2,7 +2,9 @@ package com.example.recipeapp.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Parcelize
 data class Category(
     val id: Int,
